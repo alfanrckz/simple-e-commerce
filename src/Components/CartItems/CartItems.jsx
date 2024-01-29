23ab8f0cc03c1fc4467e625c175264d1d8d0c433
alfan2911 <alfansyuriziaulhaq11@gmail.com>
2024-01-29 23:50:55 +0700

    update-frontend

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from 'react'
-import './CartItems.css'
-import { ShopContext } from '../../Context/ShopContext'
-import remove_icon from '../Assets/cart_cross_icon.png'
-import all_product from '../Assets/all_product'
-
+import React, { useContext } from "react";
+import "./CartItems.css";
+import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
-    const {all_products, cartItems, removeFromCart} = useContext(ShopContext)
+  const { getTotalCartAmmount, all_product, cartItems, removeFromCart } =
+    useContext(ShopContext);
   return (
-    <div className='cartitems'>
+    <div className="cartitems">
       <div className="cartitems-format-main">
         <p>Product</p>
         <p>Title</p>
@@ -19,25 +18,61 @@ const CartItems = () => {
       </div>
       <hr />
       {all_product.map((e) => {
-        if(cartItems[e.id]>0){
-            return(
-
-                <div>
-        <div className="cartitems-format">
-            <img src={e.image} alt="" className='carticon-product-icon' />
-            <p>{e.name}</p>
-            <p>${e.new_price}</p>
-            <button className='cartitem-quantity'>{cartItems[e.id]}</button>
-            <p>{e.new_price*cartItems[e.id]}</p>
-            <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
-        </div>
-        <hr />
-      </div>
-                )
+        if (cartItems[e.id] > 0) {
+          return (
+            <div key={e.id}>
+              <div className="cartitems-format cartitems-format-main">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitem-quantity">{cartItems[e.id]}</button>
+                <p>${e.new_price * cartItems[e.id]}</p>
+                <img
+                  className="cartitems-remove-icon"
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
         }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Total</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmmount()}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>${getTotalCartAmmount()}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a Voucher code, Enter it Here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItems
+export default CartItems;
